5886b15f55c6e043d7f1b180f704ce0463c3ac10
Ahmed Ali <imahmedalibhatti@gmail.com>
2021-02-02 21:38:01 +0500

    added todo list, todo items and visibility filter links

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="text-center text-white mb-3">
+      <h2>TodoApp</h2>
+      <h5>By Ahmed Ali BCC023929</h5>
+    </div>
+  );
+}
